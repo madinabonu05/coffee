39f5a6236365e293a8f217dc74ab57438c5bc1d0
Asadbek Abdurasulov <madina97700777@gmail.com>
2024-08-06 15:42:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let Burger = document. querySelector('.fa-bars')
+let xbtn = document. querySelector('.fa-xmark')
+
+
+Burger. addEventListener('click' ,()=>{
+    Burger.stylr.display = 'none'
+    xbtn.style.display ='inline-block'
+})
+
+xbtn. addEventListener('click' ,()=>{
+    xbtn.stylr.display = 'none'
+    xbtn.style.display ='inline-block'
+})
+
